f822bffa3873bd702bdbd32947088b3e5e4ee243
iinsue <hinsu@rmsoft.kr>
2023-01-12 16:59:08 +0900

    Update Prisma setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <div>
         <h1>메인화면</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" />
+          <button>입력</button>
+        </form>
       </div>
     </>
   );
