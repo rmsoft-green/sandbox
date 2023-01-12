e14c5158a440235104dc513dd81f69e4c1832a8c
iinsue <hinsu@rmsoft.kr>
2023-01-12 13:29:33 +0900

    mongoDB Setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
+const handleSignup = async (data: string) => {
+  const response = await fetch(`/api/hello`, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  const json = await response.json();
+  return json;
+};
+
 export default function Home() {
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(event.currentTarget[0].value);
+    try {
+      handleSignup(event.currentTarget[0].value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div>
         <h1>메인화면</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="이름을 입력하세요" />
+          <button>입력</button>
+        </form>
       </div>
     </>
   );
